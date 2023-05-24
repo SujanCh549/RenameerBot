95c8c2f0d5ba5b0fb9834dc58e32ecdb9b19f208
SujanCh549 <102373673+SujanCh549@users.noreply.github.com>
2023-05-24 16:00:07 +0545

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,11 +14,11 @@ async def start(client, message):
     user = message.from_user
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id)             
-    txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
+    txt=f"👋 Hello {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
     button=InlineKeyboardMarkup([[
         InlineKeyboardButton(" Developer ", callback_data='dev')
         ],[
-        InlineKeyboardButton('✨ Updates ✨', url='https://t.me/Thor_BotZ')
+        InlineKeyboardButton('✨ Updates ✨', url='https://t.me/Sujan_BotZ')
         ],[
         InlineKeyboardButton(' About', callback_data='about'),
         InlineKeyboardButton(' Help', callback_data='help')
@@ -67,7 +67,7 @@ async def cb_handler(client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup( [[
         InlineKeyboardButton(" Developer ", callback_data='dev')
         ],[
-        InlineKeyboardButton('✨ Updates ✨', url='https://t.me/Thor_BotZ')
+        InlineKeyboardButton('✨ Updates ✨', url='https://t.me/Sujan_BotZ')
         ],[
         InlineKeyboardButton(' About', callback_data='about'),
         InlineKeyboardButton(' Help', callback_data='help')
